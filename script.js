24bc573ac691f1ca9401b13261a43702bf222cd8
Your Name <you@example.com>
2023-08-29 13:18:51 +0800

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+var console = document.getElementById('contole');
+var input = document.getElementById('input-field')
+
+function story(text) {
+    console.innerHTML += `<p>` + text + `</p>`
+}
+function submit() {
+    story(input.value);
+    input.value = ''
+}
