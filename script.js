dc14127f1b3617c2c32fbcce6d55566f924b2c78
Your Name <you@example.com>
2024-08-30 22:31:24 +0800

    core update

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ var health = 200
 var inbattle = true
 var sayque = new Array()
 const startString = ">"
-var speechDelay = 500
+var speechDelay = 800
+var inStarterMenu = false
+var cantype = true
 // var everyCommandList = everyCommandList()
 
 var command = [
@@ -43,6 +45,31 @@ function runcommandmain(imput) {
     
 }
 
+function credit() {
+    var contribut = [
+        "acoolg",
+        "slime",
+        "christian",
+        "matthew",
+        "joseph",
+        "michael",
+        ""
+    ]
+    var times = 0
+    var intervalId = setInterval(() => {
+        // 当遍历完数组后，停止计时器并调用 startMenu
+        if (times >= contribut.length) {
+            clearInterval(intervalId);
+            clearConsole()
+            startMenu();
+        } else {
+            const nowContent = contribut[times];
+            story(nowContent);
+            times++;
+        }
+    }, speechDelay);
+}
+
 function check(text) {
     if (text.startsWith(text)) {
         return true
@@ -51,18 +78,37 @@ function check(text) {
     }
 }
 
+function handleMenu(ans) {
+    inStarterMenu = false
+    switch(ans){
+        case "1":
+            clearConsole()
+            chapter1()
+            break
+        case "3":
+            clearConsole()
+            credit()
+            break
+    }
+}
+
 function story(text) {
     sayque.push(text)
 }
 
 function submit() {
-    let ninput = input.value + '';
-    if(ninput.startsWith("/")) {
-       runcommandmain(ninput)
+    if(cantype){
+        let inputValue = input.value + '';
+        instantSay("#" + input.value);
+        input.value = '';
+        input.focus();
+        if(inStarterMenu){
+            handleMenu(inputValue)
+        }
+        if(inputValue.startsWith("/")) {
+        runcommandmain(inputValue)
+        }
     }
-    story("#" + input.value);
-    input.value = '';
-    input.focus();
 }
 
 document.addEventListener('keydown', function(e) {
@@ -83,9 +129,11 @@ function printstats() {
 setInterval(function() {
     if (sayque.length == 0) {
         console.log("nothing")
+        cantype = true;
     } else {
         consoleText.innerHTML += `<p>${startString + sayque[0]}</p>`
         sayque.shift()
+        cantype = false;
     }
 
 }, speechDelay)
@@ -101,10 +149,11 @@ function startMenu() {
  ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ 
 </pre>
     `
-    instantSayc("開始新遊戲(1)")
-    instantSayc("選擇章節(2)")
-    instantSayc("製作人員列表(3)")
-    instantSayc("抖內(4)")
+    instantSayCenter("開始新遊戲(1)")// start game
+    instantSayCenter("選擇章節(2)") // choose chapter
+    instantSayCenter("製作人員列表(3)")//auther list
+    instantSayCenter("抖內(4)")// buy us coffie
+    inStarterMenu = true
 }
 
 function chapter1() {
@@ -116,11 +165,10 @@ function chapter1() {
  ▄██▀██ ███████▄  ▄█▀██▄ ▀████████▄██████  ▄▄█▀██▀███▄███    ▀███  
 ██▀  ██ ██    ██ ██   ██   ██   ▀██  ██   ▄█▀   ██ ██▀ ▀▀      ██  
 ██      ██    ██  ▄█████   ██    ██  ██   ██▀▀▀▀▀▀ ██          ██  
-██▄    ▄██    ██ ██   ██   ██   ▄██  ██   ██▄    ▄ ██          ██
+██▄    ▄██    ██ ██   ██   ██   ▄██  ██   ██▄    ▄ ██          ██  
  █████▀████  ████▄████▀██▄ ██████▀   ▀████ ▀█████▀████▄      ▄████▄
                            ██                                      
                          ▄████▄                                    
-                                           
 
 </pre>`
     story("在一個村莊裡")
@@ -137,7 +185,7 @@ function instantSay(text) {
     consoleText.innerHTML += `<p>${text}</p>`
 }
 
-function instantSayc(text) {
+function instantSayCenter(text) {
     consoleText.innerHTML += `<p class="center">${text}</p>`
 }
 
@@ -149,3 +197,10 @@ window.onload = function() {
     input.focus
     startMenu()
 }
+
+input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        submit()
+    }
+});
